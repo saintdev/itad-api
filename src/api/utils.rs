7bed9b1e4384be2f68c42532a56c535cb51ec579
Nathan Caldwell <saintdev@gmail.com>
2021-07-21 23:24:27 -0600

    Add a serialize vec function

--- src/api/utils.rs
+++ src/api/utils.rs
@@ -11,12 +11,36 @@ where
     T: Serialize,
 {
     if let Some(value) = value {
-        let out: Vec<_> = value
-            .iter()
-            .map(|value| serde_urlencoded::to_string(value).expect("Unable to serialize value"))
-            .collect();
-        serializer.serialize_str(&out.join(","))
+        serialize_iter_urlencoded(value, serializer)
     } else {
         serializer.serialize_none()
     }
+}
+
+pub(crate) fn serialize_vec_urlencoded<S, T>(
+    value: &Option<Vec<T>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+    T: Serialize,
+{
+    if let Some(value) = value {
+        serialize_iter_urlencoded(value, serializer)
+    } else {
+        serializer.serialize_none()
+    }
+}
+
+pub(crate) fn serialize_iter_urlencoded<'a, I, S>(iter: I, serializer: S) -> Result<S::Ok, S::Error>
+where
+    I: IntoIterator,
+    I::Item: Serialize + 'a,
+    S: Serializer,
+{
+    let out: Vec<_> = iter
+        .into_iter()
+        .map(|value| serde_urlencoded::to_string(value).expect("Unable to serialize value"))
+        .collect();
+    serializer.serialize_str(&out.join("%2C"))
 }
